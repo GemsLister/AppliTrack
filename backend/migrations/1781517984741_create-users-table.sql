c875cc5b83cd7f44f2c2d4e0b7986ae4069d3918
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-06-15 18:35:58 +0800

    feat(migrations): add users table with uuid and timestamptz

---
+++ backend/migrations/1781517984741_create-users-table.sql
@@ -0,0 +1,11 @@
+-- Up Migration
+CREATE TABLE
+    users (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMPTZ DEFAULT NOW ()
+    );
+
+-- Down Migration
+DROP TABLE IF EXISTS users;
