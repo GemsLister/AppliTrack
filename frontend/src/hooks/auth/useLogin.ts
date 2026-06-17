5131cc233e639313fed8f216b2236bb75557fdbe
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-06-17 17:01:14 +0800

    feat(hooks): add useLogin hook

---
+++ frontend/src/hooks/auth/useLogin.ts
@@ -0,0 +1,33 @@
+import { api } from "../../api/axios";
+import { useState } from "react";
+
+export const useLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const login = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const { data } = await api.post("/auth/login", { email, password });
+      // Save token
+      localStorage.setItem("token", data.token);
+    } catch (error) {
+      if (error instanceof Error) setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    login,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    loading,
+    error,
+  };
+};
