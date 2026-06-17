b581d75fd504a6bf49941ce8d8e27ff4032df139
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-06-17 15:31:00 +0800

    refactor(auth): move auth.controller outside the auth folder

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { UserModel } from "../../models/auth.model";
+import { UserModel } from "../models/auth.model";
 
 // Create new user function
 export const register = async (req: Request, res: Response) => {
