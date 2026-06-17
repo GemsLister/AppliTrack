19484766705ccc69a749f168785e0034ef46a3e9
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-06-17 15:23:48 +0800

    feat(server): add auth routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import "./db";
+import authRoutes from "./routes/auth.routes";
 
 dotenv.config();
 
@@ -9,6 +11,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
