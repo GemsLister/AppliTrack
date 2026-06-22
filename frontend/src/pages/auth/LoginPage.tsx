f88deea8380e6e332a8b88f06fd1953b80a05306
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-06-22 13:32:20 +0800

    feaat(auth): add LoginPage component

---
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -0,0 +1,8 @@
+import { LoginForm } from "../../components/forms/LoginForm";
+export const LoginPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-form-bg sm:bg-auth-bg">
+      <LoginForm />
+    </div>
+  );
+};
