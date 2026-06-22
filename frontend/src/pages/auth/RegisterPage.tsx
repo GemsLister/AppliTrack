91b5a5388708c2598c8701274cbcf11e21a62d4f
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-06-22 13:33:01 +0800

    style(auth): add div with styles

--- frontend/src/pages/auth/RegisterPage.tsx
+++ frontend/src/pages/auth/RegisterPage.tsx
@@ -1,4 +1,8 @@
 import { RegisterForm } from "../../components/forms/RegisterForm";
 export const RegisterPage = () => {
-  return <RegisterForm />;
+  return (
+    <div className="flex justify-center items-center h-screen bg-form-bg sm:bg-auth-bg">
+      <RegisterForm />
+    </div>
+  );
 };
