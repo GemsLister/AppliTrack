2d5c5c20a4efc071a594aac3d5143c8c75959f5b
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-06-22 13:27:15 +0800

    feat(auth): add loading feature

--- frontend/src/components/forms/RegisterForm.tsx
+++ frontend/src/components/forms/RegisterForm.tsx
@@ -1,5 +1,7 @@
 import * as AuthHooks from "../../hooks/auth/index";
 import * as Buttons from "../buttons/index";
+import * as Inputs from "../inputs/index";
+import { LoadingAnimation } from "../../assets/icons/LoadingAnimation";
 import webLogo from "../../assets/web_logo.png";
 
 export const RegisterForm = () => {
@@ -16,66 +18,66 @@ export const RegisterForm = () => {
   } = AuthHooks.useRegister();
 
   const inputFields = [
-    {
-      label: "Username",
-      type: "text",
-      placeholder: "e.g. Juan Dela Cruz",
-      value: username,
-      onChange: setUsername,
-    },
-    {
-      label: "Email",
-      type: "email",
-      placeholder: "name@email.com",
-      value: email,
-      onChange: setEmail,
-    },
-    {
-      label: "Password",
-      type: "password",
-      placeholder: "******",
-      value: password,
-      onChange: setPassword,
-    },
+    <Inputs.InputText
+      label="Username"
+      placeholder="e.g. Juan Dela Cruz"
+      value={username}
+      onChange={setUsername}
+    />,
+    <Inputs.InputEmail
+      label="Email"
+      placeholder="name@email.com"
+      value={email}
+      onChange={setEmail}
+    />,
+    <Inputs.InputPassword
+      label="Password"
+      placeholder="******"
+      value={password}
+      onChange={setPassword}
+    />,
   ];
 
   return (
-    <div className="flex justify-center items-center h-screen bg-form-bg sm:bg-auth-bg">
-      <form className="flex flex-col items-center justify-center gap-3 sm:h-[495px] md:gap-1 w-[450px] rounded-4xl sm:shadow-2xl sm:bg-form-bg">
-        <div className="flex flex-col h-full justify-center sm:justify-around gap-3 w-full px-7">
-          <div className="flex flex-col gap-3">
-            {/* Logo and Tagline */}
-            <div className="flex flex-col w-full justify-start">
-              <img src={webLogo} alt="web-logo" className="w-58" />
-              <h1 className="font-semibold font-fredoka text-2xl text-primary">
-                Sign in and stay on track!
-              </h1>
-            </div>
-            {/* Inputs and Auth Button */}
-            <div className="flex flex-col">
-              {inputFields.map((inputs, index) => (
-                <div className="flex flex-col my-1" key={index}>
-                  <div className="flex flex-col gap-2">
-                    <label className="text-slate-600">{inputs.label}</label>
-                    <input
-                      type={inputs.type}
-                      placeholder={inputs.placeholder}
-                      className="border rounded-xl p-2 border-slate-400 text-slate-600 text-[16px]"
-                      value={inputs.value}
-                      onChange={(e) => inputs.onChange(e.target.value)}
-                    />
-                  </div>
-                </div>
-              ))}
-              <div className="mt-3">
-                <Buttons.PrimaryButton type="submit" onClick={register}>
-                  Register
-                </Buttons.PrimaryButton>
+    <form className="flex flex-col items-center justify-center gap-3 sm:h-[495px] md:gap-1 w-[450px] rounded-4xl sm:shadow-2xl sm:bg-form-bg">
+      <div className="flex flex-col h-full justify-center sm:justify-around gap-3 w-full px-7">
+        <div className="flex flex-col gap-3">
+          {/* Logo and Tagline */}
+          <div className="flex flex-col w-full justify-start">
+            <img src={webLogo} alt="web-logo" className="w-58" />
+            <h1 className="font-semibold font-fredoka text-2xl text-primary">
+              Start tracking your dream job!
+            </h1>
+          </div>
+          {/* Inputs and Auth Button */}
+          <div className="grid grid-rows-3">
+            {inputFields.map((inputs, index) => (
+              <div className="flex flex-col my-1" key={index}>
+                {inputs}
               </div>
+            ))}
+            <div className="mt-5">
+              <Buttons.PrimaryButton
+                type="submit"
+                onClick={register}
+                loading={loading}
+              >
+                {loading ? "Registering..." : "Register"}
+              </Buttons.PrimaryButton>
+            </div>
+            <div className="flex justify-center mt-3">
+              <p>
+                Already have an account?{" "}
+                <span>
+                  <a href="/" className="text-primary">
+                    Login
+                  </a>{" "}
+                </span>
+              </p>
             </div>
           </div>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 };
