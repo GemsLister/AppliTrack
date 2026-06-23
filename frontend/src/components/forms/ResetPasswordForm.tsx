2004a5b307e5b361957c8c4238d978ed1df0700e
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-06-23 18:45:25 +0800

    feat(forms): add ResetPasswordForm component

---
+++ frontend/src/components/forms/ResetPasswordForm.tsx
@@ -0,0 +1,58 @@
+import * as AuthHooks from "../../hooks/auth/index";
+import * as Buttons from "../buttons/index";
+import * as Inputs from "../inputs/index";
+import webLogo from "../../assets/web_logo.png";
+
+export const ResetPasswordForm = () => {
+  const { email, setEmail, loading, error, forgotPassword } =
+    AuthHooks.useForgotPassword();
+
+  return (
+    <form className="flex flex-col items-center justify-center gap-3 sm:h-[495px] md:gap-1 w-[450px] rounded-4xl sm:shadow-2xl sm:bg-form-bg">
+      <div className="flex flex-col h-full justify-center sm:justify-around gap-3 w-full px-7">
+        <div className="flex flex-col gap-3">
+          {/* Logo and Tagline */}
+          <div className="flex flex-col w-full justify-start">
+            <img src={webLogo} alt="web-logo" className="w-58" />
+            <h1 className="font-semibold font-fredoka text-2xl text-primary">
+              Create a strong new password
+            </h1>
+          </div>
+          {/* Inputs and Auth Button */}
+          <div>
+            <Inputs.InputPassword
+              label="New password"
+              placeholder="******"
+              value={email}
+              onChange={setEmail}
+            />
+            <Inputs.InputPassword
+              label="Confirm password"
+              placeholder="******"
+              value={email}
+              onChange={setEmail}
+            />
+          </div>
+          <div className="flex flex-col mt-3 gap-3">
+            <Buttons.PrimaryButton
+              type="submit"
+              onClick={forgotPassword}
+              loading={loading}
+            >
+              {loading ? "Loading..." : "Continue"}
+            </Buttons.PrimaryButton>
+          </div>
+          <div className="flex justify-center mt-3">
+            <p>
+              <span>
+                <a href="/forgot-password" className="text-primary">
+                  Back
+                </a>{" "}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
