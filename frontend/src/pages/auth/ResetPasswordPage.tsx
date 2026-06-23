490cc99b7813c629d3513fd084941e0efc63b6a9
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-06-23 18:51:27 +0800

    feat(pages): add ResetPasswordPage

---
+++ frontend/src/pages/auth/ResetPasswordPage.tsx
@@ -0,0 +1,8 @@
+import { ResetPasswordForm } from "../../components/forms/ResetPasswordForm";
+export const ResetPasswordPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-form-bg sm:bg-auth-bg">
+      <ResetPasswordForm />
+    </div>
+  );
+};
