8161319802626c28e5c1f2527e26a605fa519484
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-06-23 18:51:16 +0800

    feat(pages): add ForgotPasswordPage

---
+++ frontend/src/pages/auth/ForgotPasswordPage.tsx
@@ -0,0 +1,8 @@
+import { ForgotPasswordForm } from "../../components/forms/ForgotPasswordForm";
+export const ForgotPasswordPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-form-bg sm:bg-auth-bg">
+      <ForgotPasswordForm />
+    </div>
+  );
+};
